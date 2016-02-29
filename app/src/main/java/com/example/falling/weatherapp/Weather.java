2280e1e6cfd622cffa97ca7ecf56e70538c3eb4c
falling <zyz635857938@gmail.com>
2016-02-29 20:00:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/falling/weatherapp/Weather.java
@@ -0,0 +1,54 @@
+package com.example.falling.weatherapp;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by falling on 16/2/29.
+ */
+public class Weather {
+
+    public static final int TIMEOUT_MILLIS = 8000;
+
+    public String getWeather() {
+        String httpUrl = "http://apis.baidu.com/apistore/weatherservice/citylist";
+        String httpArg = "cityname=杭州";
+        String jsonResult = request(httpUrl, httpArg);
+        System.out.println(jsonResult);
+        return jsonResult;
+    }
+
+    public static String request(String httpUrl, String httpArg) {
+        BufferedReader reader = null;
+        String result = null;
+        StringBuffer sbf = new StringBuffer();
+        httpUrl = httpUrl + "?" + httpArg;
+
+        try {
+            URL url = new URL(httpUrl);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(TIMEOUT_MILLIS);
+            // 填入apikey到HTTP header
+            connection.setRequestProperty("apikey", "021d4fde5035ef8c0f3345a113a1e799");
+            connection.connect();
+            InputStream is = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String strRead = null;
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+                sbf.append("\r\n");
+            }
+            reader.close();
+            result = sbf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
