1d1a6c88cede2d4fa37b93a8285b11cbd0f14bfc
falling <zyz635857938@gmail.com>
2016-03-01 16:20:02 +0800

    实现线程周期性获取数据，点击唤醒获取数据。

--- app/src/main/java/com/example/falling/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/falling/weatherapp/MainActivity.java
@@ -5,20 +5,41 @@ import android.database.Cursor;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.falling.weatherapp.network.Weather;
 import com.example.falling.weatherapp.network.WeatherThread;
 import com.example.falling.weatherapp.provider.URIList;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextView;
+    private Button mButton;
+    private WeatherThread mWeatherThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTextView = (TextView) findViewById(R.id.View_weather);
-        new WeatherThread(this).start();
+        mButton = (Button) findViewById(R.id.getWeather_Button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getId() == R.id.getWeather_Button){
+                    //立即获取数据
+                    synchronized(mWeatherThread) {
+                        mWeatherThread.notifyAll();
+                    }
+
+                }
+            }
+        });
+
+        //开启获取天气信息的线程
+        mWeatherThread = new WeatherThread(this);
+        mWeatherThread.start();
 
 
         ContentResolver contentResolver = getContentResolver();
--- app/src/main/java/com/example/falling/weatherapp/network/WeatherThread.java
+++ app/src/main/java/com/example/falling/weatherapp/network/WeatherThread.java
@@ -2,11 +2,14 @@ package com.example.falling.weatherapp.network;
 
 import android.content.Context;
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.example.falling.weatherapp.bean.WeatherBean;
 import com.example.falling.weatherapp.database.WeatherDatabaseUtil;
 import com.example.falling.weatherapp.network.Weather;
 import com.google.gson.Gson;
+
+import javax.security.auth.login.LoginException;
 
 /**
  * Created by falling on 16/2/29.
@@ -15,12 +18,9 @@ public class WeatherThread extends Thread {
 
     public static final String SUCCESS = "success";
     private Weather mWeather;
-    private Context mContext;
     private Gson mGson;
     private WeatherDatabaseUtil mWeatherDatabaseUtil;
     public WeatherThread(Context context){
-        this.mContext = context;
-
         mGson = new Gson();
         mWeather = new Weather();
         mWeatherDatabaseUtil = new WeatherDatabaseUtil(context);
@@ -28,12 +28,24 @@ public class WeatherThread extends Thread {
 
     @Override
     public void run() {
-        WeatherBean weatherBean = mGson.fromJson(mWeather.getWeather(), WeatherBean.class);
+        while(true) {
+            WeatherBean weatherBean = mGson.fromJson(mWeather.getWeather(), WeatherBean.class);
 
-        if(TextUtils.equals(weatherBean.getErrMsg(), SUCCESS)){
-            //insert into database;
-            mWeatherDatabaseUtil.insert(weatherBean);
+            Log.i("Tag","获取一次");
+            if (TextUtils.equals(weatherBean.getErrMsg(), SUCCESS)) {
+                //insert into database;
+                mWeatherDatabaseUtil.insert(weatherBean);
+            }
 
+            synchronized(this) {
+                try {
+                    //每隔5秒（示例作用，实际可改为1小时）获取一次网络信息写入数据库。
+                    //可通过按钮来唤醒，提前获取数据
+                    this.wait(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
 
